127fd18d33c35690eb550437d0d0778fd38c5d0c
민우석 <s23044@gsm.hs.kr>
2025-02-01 21:12:35 +0900

    🐛 Add: use client

--- src/app/_provider/TanstackProvider.tsx
+++ src/app/_provider/TanstackProvider.tsx
@@ -1,8 +1,10 @@
+'use client';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 
 function TanstackProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = React.useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient());
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
 
