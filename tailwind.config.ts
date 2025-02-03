1402bac69e2e1f42ea3f08d45e9c2319665f8464
happytaeyoon <s23021@gsm.hs.kr>
2025-02-03 16:37:32 +0900

    ✨ : change fontSize title

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,22 +35,22 @@ const config: Config = {
         success: 'var(--color-success',
       },
       fontSize: {
-        h1: ['2.5rem', { lineHeight: '3rem', fontWeight: '500' }], // Title 1 (Medium)
-        h2: ['2.25rem', { lineHeight: '2.7rem', fontWeight: '500' }], // Title 2 (Medium)
-        h3: ['2rem', { lineHeight: '2.4rem', fontWeight: '600' }], // Title 3 (SemiBold)
-        h4: ['2rem', { lineHeight: '2.4rem', fontWeight: '400' }], // Title 3 (Regular)
-        body1: ['1.5rem', { lineHeight: '1.8rem', fontWeight: '600' }], // Body 1 (SemiBold)
-        body2: ['1.5rem', { lineHeight: '1.8rem', fontWeight: '400' }], // Body 1 (Regular)
-        body3: ['1.25rem', { lineHeight: '1.5rem', fontWeight: '600' }], // Body 2 (SemiBold)
-        body4: ['1.25rem', { lineHeight: '1.5rem', fontWeight: '400' }], // Body 2 (Regular)
-        body5: ['1rem', { lineHeight: '1.4rem', fontWeight: '600' }], // Body 3 (SemiBold)
-        body6: ['1rem', { lineHeight: '1.6rem', fontWeight: '400' }], // Body 3 (Regular)
-        caption1: ['0.875rem', { lineHeight: '1.225rem', fontWeight: '600' }], // Caption 1 (SemiBold)
-        caption2: ['0.875rem', { lineHeight: '1.225rem', fontWeight: '400' }], // Caption 1 (Regular)
-        caption3: ['0.75rem', { lineHeight: '1.125rem', fontWeight: '600' }], // Caption 2 (SemiBold)
-        caption4: ['0.75rem', { lineHeight: '1.125rem', fontWeight: '500' }], // Caption 2 (Medium)
-        caption5: ['0.625rem', { lineHeight: '1.125rem', fontWeight: '600' }], // Caption 3 (SemiBold)
-        caption6: ['0.625rem', { lineHeight: '1.125rem', fontWeight: '500' }], // Caption 3 (Medium)
+        title1M: ['2.5rem', { lineHeight: '3rem', fontWeight: '500' }], // Title 1 (Medium)
+        title2M: ['2.25rem', { lineHeight: '2.7rem', fontWeight: '500' }], // Title 2 (Medium)
+        title3B: ['2rem', { lineHeight: '2.4rem', fontWeight: '600' }], // Title 3 (SemiBold)
+        title3R: ['2rem', { lineHeight: '2.4rem', fontWeight: '400' }], // Title 3 (Regular)
+        body1B: ['1.5rem', { lineHeight: '1.8rem', fontWeight: '600' }], // Body 1 (SemiBold)
+        body1R: ['1.5rem', { lineHeight: '1.8rem', fontWeight: '400' }], // Body 1 (Regular)
+        body2B: ['1.25rem', { lineHeight: '1.5rem', fontWeight: '600' }], // Body 2 (SemiBold)
+        body2R: ['1.25rem', { lineHeight: '1.5rem', fontWeight: '400' }], // Body 2 (Regular)
+        body3B: ['1rem', { lineHeight: '1.4rem', fontWeight: '600' }], // Body 3 (SemiBold)
+        body3R: ['1rem', { lineHeight: '1.6rem', fontWeight: '400' }], // Body 3 (Regular)
+        caption1B: ['0.875rem', { lineHeight: '1.225rem', fontWeight: '600' }], // Caption 1 (SemiBold)
+        caption1R: ['0.875rem', { lineHeight: '1.225rem', fontWeight: '400' }], // Caption 1 (Regular)
+        caption2B: ['0.75rem', { lineHeight: '1.125rem', fontWeight: '600' }], // Caption 2 (SemiBold)
+        caption2M: ['0.75rem', { lineHeight: '1.125rem', fontWeight: '500' }], // Caption 2 (Medium)
+        caption3B: ['0.625rem', { lineHeight: '1.125rem', fontWeight: '600' }], // Caption 3 (SemiBold)
+        caption3M: ['0.625rem', { lineHeight: '1.125rem', fontWeight: '500' }], // Caption 3 (Medium)
       },
       fontFamily: {
         Pretendard: ['Pretendard'],
