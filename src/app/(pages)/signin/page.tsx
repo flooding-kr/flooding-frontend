e576c4b451728b5a285ef5f2aab798202110a475
happytaeyoon <s23021@gsm.hs.kr>
2025-02-03 15:49:59 +0900

    🎨 Add: input component

---
+++ src/app/(pages)/signin/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import Input from '@/shared/ui/Input';
+
+export default function SignIn() {
+  type FormType = {
+    email: string;
+    password: string;
+  };
+  const onSubmit = (data: FormType) => {
+    // eslint-disable-next-line
+    console.log(data);
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormType>();
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          {...register('email', {
+            required: '이메일을 입력해주세요.',
+          })}
+          placeholder="이메일을 입력해주세요"
+          error={errors.email?.message}
+        />
+        <Input
+          {...register('password', {
+            required: '비밀번호를 입력해주세요.',
+            pattern: {
+              value: /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/,
+              message: '비밀번호는 8자리 이상, 대문자 1개, 특수문자 1개 이상을 포함해야 합니다.',
+            },
+          })}
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          error={errors.password?.message}
+        />
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ src/shared/ui/Input/index.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { DetailedHTMLProps, InputHTMLAttributes, forwardRef, useState } from 'react';
+
+import { Eye, EyeClose } from '@/shared/assets/icons';
+
+interface Props extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+  error?: string;
+}
+
+const Input = forwardRef<HTMLInputElement, Props>(
+  ({ error, type = 'text', placeholder, onChange, value, required, ...props }, ref) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+      setIsPasswordVisible(prev => !prev);
+    };
+
+    let inputType = type;
+    if (type === 'password') {
+      inputType = isPasswordVisible ? 'text' : 'password';
+    }
+
+    const inputStyle = {
+      WebkitBoxShadow: '0 0 0 30px white inset !important',
+      WebkitTextFillColor: 'inherit !important',
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (onChange) {
+        onChange(e);
+      }
+    };
+
+    const inputId = `input-${Math.random().toString(36).substr(2, 9)}`;
+
+    return (
+      <div>
+        <div className="relative w-full rounded-md border-[3px] p-[16px] bg-white">
+          <div className="w-full">
+            <label htmlFor={inputId} className="flex duration-200">
+              <input
+                {...props}
+                id={inputId}
+                ref={ref}
+                type={inputType}
+                className="w-full text-gray-500 text-body2 "
+                style={inputStyle}
+                placeholder={`${placeholder}`}
+                onChange={handleChange}
+                value={value}
+              />
+            </label>
+            {type === 'password' && (
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute right-[16px] top-1/2 transform -translate-y-1/2"
+              >
+                {isPasswordVisible ? <Eye /> : <EyeClose />}
+              </button>
+            )}
+          </div>
+        </div>
+        {error && <div className="mt-2 flex text-sm text-error">{error}</div>}
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+export default Input;
