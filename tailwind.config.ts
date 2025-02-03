c0499c5717faca9b9eca472a3b6a6d629c9be33b
happytaeyoon <s23021@gsm.hs.kr>
2025-02-03 20:33:28 +0900

    ✨ : Add tailwind path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/shared/**/*.{js,ts,jsx,tsx}',
+    './src/views/**/*.{js,ts,jsx,tsx}',
+    './src/widgets/**/*.{js,ts,jsx,tsx}',
+    './src/features/**/*.{js,ts,jsx,tsx}',
+    './src/entities/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
