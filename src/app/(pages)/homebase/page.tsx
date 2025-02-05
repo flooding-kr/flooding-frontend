9c5c3d360525a498c6f4fc002196dd06b37aee58
민우석 <s23044@gsm.hs.kr>
2025-02-05 16:12:12 +0900

    ✨ Add: search input components

--- src/app/(pages)/homebase/page.tsx
+++ src/app/(pages)/homebase/page.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+'use client';
 
-function HomeBase() {
-  return <div>homebase</div>;
+// import useStore from '@/shared/stores/useStore';
+import { useState } from 'react';
+
+import SearchInput from '@/shared/ui/SearchInput';
+
+export default function HomeBase() {
+  const [s, u] = useState('');
+
+  return (
+    <div>
+      <SearchInput
+        placeholder="검색할 학생"
+        onChange={e => u(e.target.value)}
+        onDebounce={value => {
+          // eslint-disable-next-line no-console
+          console.log(`검색:${value}`);
+        }}
+        value={s}
+        debounceTime={300}
+      />
+    </div>
+  );
 }
-
-export default HomeBase;
