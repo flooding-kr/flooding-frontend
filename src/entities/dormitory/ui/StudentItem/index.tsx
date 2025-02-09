95e799ce66ba2bd9ebcc7666ab96500bece2fd20
민우석 <s23044@gsm.hs.kr>
2025-02-09 23:56:48 +0900

    ✨ Add: student item

---
+++ src/entities/dormitory/ui/StudentItem/index.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import React from 'react';
+
+import userProfileImage from '@/shared/assets/jpg/userProfileImage.jpg';
+
+interface Props {
+  stuNum: number;
+  stuName: string;
+}
+
+export default function StudentItem({ stuNum, stuName }: Props) {
+  return (
+    <div className="w-[120px] h-[134px] flex justify-center items-center border-[1px] border-gray-200 border-solid rounded-lg">
+      <div className="flex flex-col items-center gap-3">
+        <Image alt="profile" src={userProfileImage} className="w-12 h-12 rounded-full flex" />
+        <div className="flex flex-col items-center gap-1">
+          <p className="text-body3R text-gray-500">{stuNum}</p>
+          <p className="text-body2B text-black">{stuName}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
