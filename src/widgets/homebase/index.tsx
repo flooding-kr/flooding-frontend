dfe0b371ee0b22c93ee4b5d678c66922c74952df
happytaeyoon <s23021@gsm.hs.kr>
2025-02-12 16:04:35 +0900

    ✨ Add: HomeBaserPageWrapper

---
+++ src/widgets/homebase/index.tsx
@@ -0,0 +1,1 @@
+export { default as HomeBasePageWrapper } from './ui/HomeBasePageWrapper';
---
+++ src/widgets/homebase/ui/HomeBasePageWrapper/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HomeBasePageWrapper() {
+  return <div className="w-full max-w-[1360px]"></div>;
+}
