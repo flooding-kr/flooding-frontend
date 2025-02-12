758ecfb4e0f7c991b2042f3b7739dff8f3ad9cac
happytaeyoon <s23021@gsm.hs.kr>
2025-02-12 16:21:51 +0900

    ✨ Add: VerticalaLineBig svg

---
+++ src/shared/assets/svg/VerticalLineBig.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function VerticalLineBig() {
+  return (
+    <svg width="3" height="24" viewBox="0 0 3 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line x1="1.5" y1="4.37114e-08" x2="1.5" y2="24" stroke="#F4F4F4" stroke-width="2" />
+    </svg>
+  );
+}
