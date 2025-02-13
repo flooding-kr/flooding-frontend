c84cc43662312b38da3a369e05c94a54080d8f9d
happytaeyoon <s23021@gsm.hs.kr>
2025-02-13 15:44:05 +0900

    ✨ Add: svgs

--- src/shared/ui/Header/index.tsx
+++ src/shared/ui/Header/index.tsx
@@ -1,5 +1,15 @@
-import { HeaderLogo } from '@/shared/assets/svg';
 import React from 'react';
+
+import { HeaderLogo } from '@/shared/assets/svg';
+import {
+  HeaderAttendance,
+  HeaderClub,
+  HeaderDormitory,
+  HeaderHomebase,
+  HeaderManager,
+  HeaderNotification,
+  HeaderTotal,
+} from '@/shared/assets/icons';
 
 export default function Header() {
   return (
@@ -9,18 +19,42 @@ export default function Header() {
           <HeaderLogo />
           <div className="flex gap-10">
             <div className="flex gap-6">
-              <p>로고</p>
-              <p>로고</p>
+              <div className="flex gap-2">
+                <HeaderManager isSelected={false} />
+                <p>관리자</p>
+              </div>
+              <div className="flex gap-2">
+                <HeaderNotification isSelected={false} />
+                <p>공지</p>
+              </div>
             </div>
-            <p>로고</p>
+            <div className="flex gap-5">
+              <HeaderTotal isSelected={false} />
+              <p>김진원</p>
+            </div>
           </div>
         </div>
         <div className="flex gap-10">
-          <p>로고</p>
-          <p>로고</p>
-          <p>로고</p>
-          <p>로고</p>
-          <p>로고</p>
+          <div className="flex gap-3">
+            <HeaderTotal isSelected={false} />
+            <p>전체</p>
+          </div>
+          <div className="flex gap-3">
+            <HeaderDormitory isSelected={false} />
+            <p>기숙사</p>
+          </div>
+          <div className="flex gap-3">
+            <HeaderHomebase isSelected={false} />
+            <p>홈베이스</p>
+          </div>
+          <div className="flex gap-3">
+            <HeaderClub isSelected={false} />
+            <p>동아리</p>
+          </div>
+          <div className="flex gap-3">
+            <HeaderAttendance isSelected={false} />
+            <p>출결</p>
+          </div>
         </div>
       </div>
     </div>
