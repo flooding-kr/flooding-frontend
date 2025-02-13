cec045f614e0281e652259b0a0ec6186e53e152a
happytaeyoon <s23021@gsm.hs.kr>
2025-02-13 19:58:46 +0900

    ✨ Add: mobile footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 import { usePathname } from 'next/navigation';
 
 import { pretendard } from '@/shared/style/font';
-import { Header } from '@/shared/ui';
+import { Header, MoblieFooter } from '@/shared/ui';
 
 import TanstackProvider from './_provider/TanstackProvider';
 import './globals.css';
@@ -14,13 +14,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
+  const isAuthPage = pathname === '/signin' || pathname === '/signup';
 
   return (
     <html lang="ko">
       <body className={pretendard.className}>
         <TanstackProvider>
-          {pathname === '/signin' || pathname === '/signup' ? null : <Header />}
-          {children}
+          {!isAuthPage && <Header />}
+          <div className="mobile:pt-[137px] mobile:pb-[63px]">{children}</div>
+          {!isAuthPage && <MoblieFooter />}
         </TanstackProvider>
       </body>
     </html>
