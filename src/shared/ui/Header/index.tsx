97f98254be597837d4cf1d808230db36ca2f120b
happytaeyoon <s23021@gsm.hs.kr>
2025-02-13 15:23:39 +0900

    ✨ Add: HeaderLogo

--- src/shared/ui/Header/index.tsx
+++ src/shared/ui/Header/index.tsx
@@ -1,3 +1,4 @@
+import { HeaderLogo } from '@/shared/assets/svg';
 import React from 'react';
 
 export default function Header() {
@@ -5,7 +6,7 @@ export default function Header() {
     <div className="flex justify-center w-full bg-main-600">
       <div className="flex flex-col w-full max-w-[1360px] my-6 gap-9">
         <div className="flex justify-between">
-          <p>로고</p>
+          <HeaderLogo />
           <div className="flex gap-10">
             <div className="flex gap-6">
               <p>로고</p>
