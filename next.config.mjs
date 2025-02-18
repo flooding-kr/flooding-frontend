00a468c4df937c7504fa24a06e0624d9869fe3c5
민우석 <s23044@gsm.hs.kr>
2025-02-18 21:19:35 +0900

    ✨ Add: get schedule api

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,11 @@ const nextConfig = {
     return [
       {
         source: '/neis/meal/:date/:code',
-        destination: `https://open.neis.go.kr/hub/mealServiceDietInfo?Key=${process.env.NEXT_PUBLIC_NEIS_KEY}&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MMEAL_SC_CODE=3&MLSV_YMD=:date&MMEAL_SC_CODE=:code`,
+        destination: `https://open.neis.go.kr/hub/mealServiceDietInfo?Key=${process.env.NEXT_PUBLIC_NEIS_KEY}&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=:date&MMEAL_SC_CODE=:code`,
+      },
+      {
+        source: '/neis/schedule/:date/:grade/:class',
+        destination: `https://open.neis.go.kr/hub/hisTimetable?Key=${process.env.NEXT_PUBLIC_NEIS_KEY}&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&pIndex=1&pSize=7&ALL_TI_YMD=:date&CLASS_NM=:class&GRADE=:grade`,
       },
     ];
   },
