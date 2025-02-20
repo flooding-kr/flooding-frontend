e8924eccff2f718cce18c2476b893fc13fa52321
happytaeyoon <s23021@gsm.hs.kr>
2025-02-20 21:50:03 +0900

    ✨ Feat: apiClient

---
+++ src/shared/libs/apiClient.ts
@@ -0,0 +1,49 @@
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+
+interface TokenResponse {
+  newAuthorization: string;
+}
+
+export const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.response.use(
+  response => response,
+  async (err: AxiosError) => {
+    const originalRequest = err.config as AxiosRequestConfig & {
+      retryAttempted?: boolean;
+    };
+
+    if (err.response && err.response.status === 401 && !originalRequest.retryAttempted) {
+      originalRequest.retryAttempted = true;
+      try {
+        const refreshToken = localStorage.getItem('refreshToken');
+        if (!refreshToken) {
+          window.location.href = '/signin';
+          return await Promise.reject(new Error('No refresh token found'));
+        }
+
+        const tokenResponse = await axios.patch<TokenResponse>('/api/auth/tokenReissue', null, {
+          headers: { Authorization: `Bearer ${refreshToken}` },
+        });
+
+        if (tokenResponse.data && tokenResponse.data.newAuthorization) {
+          localStorage.setItem('accessToken', tokenResponse.data.newAuthorization);
+          originalRequest.headers = originalRequest.headers || {};
+          originalRequest.headers.Authorization = `Bearer ${tokenResponse.data.newAuthorization}`;
+
+          return await apiClient(originalRequest);
+        }
+      } catch (refreshError) {
+        window.location.href = '/signin';
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(err);
+  }
+);
