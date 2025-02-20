9559b7f740beec55b4da66466656772d44a887b6
민우석 <s23044@gsm.hs.kr>
2025-02-21 03:59:39 +0900

    📱 Add: responsive dormitory panel

--- src/shared/assets/svg/VerticalLineBig.tsx
+++ src/shared/assets/svg/VerticalLineBig.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function VerticalLineBig() {
   return (
     <svg width="3" height="24" viewBox="0 0 3 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <line x1="1.5" y1="4.37114e-08" x2="1.5" y2="24" stroke="#F4F4F4" stroke-width="2" />
+      <line x1="1.5" y1="4.37114e-08" x2="1.5" y2="24" stroke="#F4F4F4" strokeWidth="2" />
     </svg>
   );
 }
