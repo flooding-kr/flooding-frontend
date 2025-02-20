3adc217625fc64f023a59381e6d0b35db4c721ae
민우석 <s23044@gsm.hs.kr>
2025-02-21 04:00:38 +0900

    📱 Add: responsive homepage

--- src/views/home/ui/index.tsx
+++ src/views/home/ui/index.tsx
@@ -1,12 +1,15 @@
+import { SectionTitle } from '@/entities/homebase';
 import { DormitoryPanel, OverviewPanel } from '@/widgets/home';
 import { HomeBasePageWrapper } from '@/widgets/homebase';
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center gap-12 pt-[47px] pb-[76px]">
+    <div className="w-full flex flex-col items-center gap-12 pt-12 pb-20 mobile:gap-6 mobile:p-4">
       <OverviewPanel />
       <DormitoryPanel />
-      <HomeBasePageWrapper />
+      <div className="w-full flex justify-center mobile:hidden">
+        <HomeBasePageWrapper />
+      </div>
     </div>
   );
 }
