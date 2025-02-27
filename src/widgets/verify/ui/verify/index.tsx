16117be87320d4b5aa421dea1d80d095c97609bd
happytaeyoon <s23021@gsm.hs.kr>
2025-02-27 17:31:56 +0900

    🐛 Fix: Add Suspense to handle useSearchParams warning

--- src/widgets/verify/ui/verify/index.tsx
+++ src/widgets/verify/ui/verify/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { Verify } from '@/entities/verify';
 
 export default function VerifyContainer() {
   return (
-    <div className="w-full text-center max-w-[666px] px-4">
-      <Verify />
-    </div>
+    <Suspense>
+      <div className="w-full text-center max-w-[666px] px-4">
+        <Verify />
+      </div>
+    </Suspense>
   );
 }
