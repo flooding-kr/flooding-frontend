352f4830837d9144f3b4b7f00384a2e954c356cf
민우석 <s23044@gsm.hs.kr>
2025-03-06 15:19:33 +0900

    ✨ Add: change over time meal

--- src/entities/home/ui/MealBoard/index.tsx
+++ src/entities/home/ui/MealBoard/index.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { ArrowLeft, ArrowRight, Error } from '@/shared/assets/icons';
+import { Meal } from '@/shared/types/meal';
 
+import { getMeal } from '../../api';
 import { getDate } from '../../api/getDate';
-import { getMeal } from '../../api/getMeal';
 
 export default function MealBoard() {
-  const { year, month, day, weekday } = getDate();
+  const { year, month, day, weekday, hour } = getDate();
   const [dailyMeal, setDailyMeal] = useState(0);
   const [date, setDate] = useState({ year, month, day, weekday });
+  const [menu, setMenu] = useState<Meal | null>(null);
+  const initialRef = useRef(true);
 
   const currentDate = `${date.year}${String(date.month).padStart(2, '0')}${String(date.day).padStart(2, '0')}`;
-  const { meal } = getMeal({ currentDate, MMEAL_SC_CODE: dailyMeal + 1 });
 
   const dailyMealArr = ['조식', '중식', '석식'];
   const weekArr = ['일', '월', '화', '수', '목', '금', '토'];
@@ -30,9 +32,39 @@ export default function MealBoard() {
   const handleMealChange = (idx: number) => {
     setDailyMeal(idx);
   };
+  useEffect(() => {
+    if (initialRef.current) {
+      initialRef.current = false;
+      return;
+    }
+
+    const fetchMeal = async () => {
+      try {
+        const meal = await getMeal({ currentDate, MMEAL_SC_CODE: dailyMeal + 1 });
+        setMenu(meal);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchMeal();
+  }, [currentDate, dailyMeal]);
+
+  useEffect(() => {
+    if (hour >= 19) {
+      handleDateChange(1);
+      setDailyMeal(0);
+    } else if (hour >= 13) {
+      setDailyMeal(2);
+    } else if (hour >= 8) {
+      setDailyMeal(1);
+    } else {
+      setDailyMeal(0);
+    }
+  }, [hour]);
 
   return (
-    <section className="bg-white rounded-lg px-8 py-6 max-w-[548px] h-[418px] w-full flex flex-col mobile:max-w-[484px] mobile:min-h-[243px] mobile:max-h-[260px] mobile:h-fit mobile:p-3">
+    <section className="bg-white rounded-lg px-8 py-6 max-w-[548px] h-[418px] w-full flex flex-col mobile:max-w-[484px] mobile:min-h-[243px] mobile:max-h-[260px] mobile:h-fit mobile:p-3 tablet:max-w-full">
       <div className="flex flex-col gap-6 flex-1 mobile:gap-4">
         <div className="flex flex-col gap-6 flex-1 border-b-[1px] border-gray-200 border-solid mobile:gap-4 mobile:border-0">
           <div className="flex flex-col gap-6 mobile:gap-[13px]">
@@ -53,7 +85,7 @@ export default function MealBoard() {
                 <button
                   type="button"
                   className="flex justify-center items-center"
-                  onClick={() => handleDateChange(+1)}
+                  onClick={() => handleDateChange(1)}
                 >
                   <ArrowRight color="#121212" />
                 </button>
@@ -64,7 +96,7 @@ export default function MealBoard() {
                 <button
                   key={item}
                   type="button"
-                  className={`px-12 mobile:px-[31px] py-4 mobile:py-3 rounded-lg transition-colors duration-300 ease-in-out ${
+                  className={`px-12 py-4 mobile:flex mobile:justify-center mobile:items-center tablet:px-[27px] mobile:py-3 mobile:max-w-[28%] rounded-lg transition-colors duration-300 ease-in-out ${
                     dailyMeal === idx ? 'bg-main-600' : ''
                   }`}
                   disabled={dailyMeal === idx}
@@ -76,26 +108,28 @@ export default function MealBoard() {
             </div>
           </div>
           <div className="flex-1 flex justify-center">
-            {meal ? (
-              <div className="flex flex-wrap gap-3 h-full justify-start">
-                {meal.DDISH_NM.split('<br/>').map(dish => (
-                  <p key={dish} className="w-[40%] text-body3R text-black mobile:text-caption1R">
-                    {dish.split(' ')[0]}
-                  </p>
-                ))}
-              </div>
-            ) : (
-              <div className="w-full h-full flex items-center justify-center">
-                <div className="flex flex-col justify-center items-center gap-4">
-                  <Error color="#A7A7A7" />
-                  <p className="text-body2R text-gray-500">오늘은 급식이 없습니다.</p>
+            <div>
+              {menu ? (
+                <div className="flex flex-wrap gap-3 h-full justify-start">
+                  {menu.DDISH_NM.split('<br/>').map(dish => (
+                    <p key={dish} className="w-[40%] text-body3R text-black mobile:text-caption1R">
+                      {dish.split(' ')[0]}
+                    </p>
+                  ))}
                 </div>
-              </div>
-            )}
+              ) : (
+                <div className="w-full h-full flex items-center justify-center">
+                  <div className="flex flex-col justify-center items-center gap-4">
+                    <Error color="#A7A7A7" />
+                    <p className="text-body2R text-gray-500">오늘은 급식이 없습니다.</p>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
         <footer className="flex justify-end mobile:hidden">
-          <p className="text-body2R text-main-400">{meal?.CAL_INFO ?? '0 Kcal'}</p>
+          <p className="text-body2R text-main-400">{menu?.CAL_INFO ?? '0 Kcal'}</p>
         </footer>
       </div>
     </section>
