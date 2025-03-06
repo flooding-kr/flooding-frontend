c6461a5712b69cf9b656e4fe4d683d31f190e7ce
민우석 <s23044@gsm.hs.kr>
2025-03-06 15:18:31 +0900

    🔥 Delete: not fit the api

--- src/entities/home/api/getMeal.ts
+++ src/entities/home/api/getMeal.ts
@@ -1,32 +1,11 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-import { Meal } from '@/shared/types/meal';
 
 interface Props {
   currentDate: string;
   MMEAL_SC_CODE: number;
 }
 
-export const getMeal = ({ currentDate, MMEAL_SC_CODE }: Props) => {
-  const [meal, setMeal] = useState<Meal | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchMeal = async () => {
-      try {
-        const { data } = await axios.get(`neis/meal/${currentDate}/${MMEAL_SC_CODE}`);
-        setMeal(data.mealServiceDietInfo?.[1]?.row?.[0]);
-      } catch (err) {
-        setMeal(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchMeal();
-  }, [currentDate, MMEAL_SC_CODE]);
-
-  return { meal, loading };
+export const getMeal = async ({ currentDate, MMEAL_SC_CODE }: Props) => {
+  const { data } = await axios.get(`neis/meal/${currentDate}/${MMEAL_SC_CODE}`);
+  return data.mealServiceDietInfo?.[1]?.row?.[0] || null;
 };
