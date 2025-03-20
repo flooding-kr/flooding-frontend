f008cc3678c28da49b90b3a14c0658d31a1f1737
민우석 <s23044@gsm.hs.kr>
2025-03-20 20:42:46 +0900

    💚 Fix: suspense boundary

--- src/widgets/resetPassword/ui/index.tsx
+++ src/widgets/resetPassword/ui/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import ChangePasswordForm from '@/entities/resetPassword/ui/ResetPasswordForm';
 import { SignInLogo } from '@/shared/assets/svg';
 
 export default function ResetPassword() {
   return (
-    <div className="max-w-[588px] w-full flex flex-col h-screen items-center justify-center gap-10">
-      <div className="flex w-full justify-center">
-        <SignInLogo />
+    <Suspense>
+      <div className="max-w-[588px] w-full flex flex-col h-screen items-center justify-center gap-10">
+        <div className="flex w-full justify-center">
+          <SignInLogo />
+        </div>
+        <ChangePasswordForm />
       </div>
-      <ChangePasswordForm />
-    </div>
+    </Suspense>
   );
 }
