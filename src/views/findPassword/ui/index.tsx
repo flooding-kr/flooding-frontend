200803cc128a79d4323fdf3bbb2b6a62bea4ebff
민우석 <s23044@gsm.hs.kr>
2025-03-20 16:18:37 +0900

    🎨 Add: findpassword page

---
+++ src/views/findPassword/ui/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { FindEmail } from '@/widgets/findPassword';
+
+export default function FindPasswordPositioner() {
+  return (
+    <div className="w-full flex justify-center">
+      <FindEmail />
+    </div>
+  );
+}
