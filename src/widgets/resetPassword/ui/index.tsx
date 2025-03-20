99eddd782f0d0c069380d48b7cf1274a850c46c1
민우석 <s23044@gsm.hs.kr>
2025-03-20 18:25:43 +0900

    🎨 Add: reset password page

---
+++ src/widgets/resetPassword/ui/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ChangePasswordForm from '@/entities/resetPassword/ui/ResetPasswordForm';
+import { SignInLogo } from '@/shared/assets/svg';
+
+export default function ResetPassword() {
+  return (
+    <div className="max-w-[588px] w-full flex flex-col h-screen items-center justify-center gap-10">
+      <div className="flex w-full justify-center">
+        <SignInLogo />
+      </div>
+      <ChangePasswordForm />
+    </div>
+  );
+}
