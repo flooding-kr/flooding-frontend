2e105e88a7092924247fa05e22bed34c0d2a2d09
민우석 <s23044@gsm.hs.kr>
2025-03-31 23:47:25 +0900

    ✨ Change: schedule api

---
+++ src/app/api/neis/schedule/[date]/route.ts
@@ -0,0 +1,31 @@
+import { AxiosError } from 'axios';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+import { apiClient } from '@/shared/libs/apiClient';
+
+export async function GET(request: Request, { params }: { params: { date: string } }) {
+  const { date } = params;
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+
+  const config = accessToken
+    ? {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    : {};
+
+  try {
+    const response = await apiClient.get(`/neis/timetable?date=${date}`, config);
+    return NextResponse.json(response.data.periods);
+  } catch (error) {
+    const axiosError = error as AxiosError<{ message: string }>;
+
+    const status = axiosError.response?.status || 500;
+    const message = axiosError.response?.data?.message || 'get schedule failed';
+
+    return NextResponse.json({ error: message }, { status });
+  }
+}
