f6a59e255ec012bc903f0e3fab8e1ff168cbadb6
민우석 <s23044@gsm.hs.kr>
2025-04-14 00:51:23 +0900

    🔥 Delete: pending state

--- src/entities/club/ui/clubState/index.tsx
+++ src/entities/club/ui/clubState/index.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 
 interface Props {
-  state?: string;
+  state?: boolean;
 }
 
 function ClubState({ state }: Props) {
-  let stateStyle = 'absolute w-fit rounded-lg px-5 py-2';
-
-  switch (state) {
-    case 'PENDING':
-      stateStyle += ' bg-[#DCB800]';
-      break;
-    case 'RECRUITING':
-      stateStyle += ' bg-[#1EB916]';
-      break;
-    default:
-      stateStyle = 'hidden';
-  }
-
   return (
-    <div className={stateStyle}>
-      <p className="text-caption1B text-white">{state === 'PENDING' ? '대기' : '모집'}</p>
+    <div className={`absolute w-fit rounded-lg px-5 py-2 bg-[#1EB916] ${state && 'hidden'}`}>
+      <p className="text-caption1B text-white">모집</p>
     </div>
   );
 }
