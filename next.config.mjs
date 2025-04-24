9b5f7bcccd6106278a27b00fa31d79b1ee424d97
민우석 <s23044@gsm.hs.kr>
2025-04-24 12:03:55 +0900

    ✨ Add: get server meal

--- next.config.mjs
+++ next.config.mjs
@@ -6,27 +6,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'pub-e40de018bd184237aa27f7a32ee8dcf3.r2.dev',
-        pathname: '/**',
+        hostname: 'flooding-bucket.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/images/*',
       },
-      {
-        protocol: 'https',
-        hostname: 'r2.flooding.kr',
-        pathname: '/**',
-      }
     ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/neis/meal/:date/:code',
-        destination: `https://open.neis.go.kr/hub/mealServiceDietInfo?Key=${process.env.NEXT_PUBLIC_NEIS_KEY}&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=:date&MMEAL_SC_CODE=:code`,
-      },
-      {
-        source: '/neis/schedule/:date/:grade/:class',
-        destination: `https://open.neis.go.kr/hub/hisTimetable?Key=${process.env.NEXT_PUBLIC_NEIS_KEY}&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&pIndex=1&pSize=7&ALL_TI_YMD=:date&CLASS_NM=:class&GRADE=:grade`,
-      },
-    ];
   },
 };
 
