239f4ea37c7d737b47820f13a2f982f23a04401c
민우석 <s23044@gsm.hs.kr>
2025-04-24 13:40:33 +0900

    ✨ Add: schedule params

--- src/entities/home/model/useSchedule.ts
+++ src/entities/home/model/useSchedule.ts
@@ -1,14 +1,23 @@
 import { useState, useCallback } from 'react';
 
+import useUser from '@/shared/hooks/useUser';
+
 import { getSchedule } from '../api/getSchedule';
 
 export const useSchedule = () => {
   const [schedule, setSchedule] = useState<string[] | null>();
-
-  const fetchSchedule = useCallback(async (date: string) => {
-    const data = await getSchedule({ date });
-    setSchedule(data);
-  }, []);
+  const user = useUser();
+  const classroom = user?.student_info.classroom;
+  const grade = user?.student_info.grade;
+  const fetchSchedule = useCallback(
+    async (date: string) => {
+      if (grade && classroom) {
+        const data = await getSchedule({ date, grade, classroom });
+        setSchedule(data);
+      }
+    },
+    [grade, classroom]
+  );
 
   return { schedule, fetchSchedule };
 };
