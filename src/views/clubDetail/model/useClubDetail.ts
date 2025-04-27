6543d9e131590040759b8dcd93d59bd86b02dd64
민우석 <s23044@gsm.hs.kr>
2025-04-27 14:49:35 +0900

    ✨ Add: get club detail

---
+++ src/views/clubDetail/model/useClubDetail.ts
@@ -0,0 +1,27 @@
+import { useParams } from 'next/navigation';
+import { useCallback, useState } from 'react';
+
+import { ClubDetail } from '@/shared/types/club';
+
+import { getClubDetail } from '../api/getClubDetail';
+
+function useClubDetail() {
+  const [detailData, setDetailData] = useState<ClubDetail | null>(null);
+
+  const params = useParams();
+  const id = typeof params.id === 'string' ? params.id : params.id?.[0];
+
+  const fetchClubDetail = useCallback(async () => {
+    if (!id) return;
+    try {
+      const { data } = await getClubDetail({ id });
+      setDetailData(data);
+    } catch (error) {
+      console.error('Failed to fetch club detail', error);
+    }
+  }, []);
+
+  return { detailData, fetchClubDetail };
+}
+
+export default useClubDetail;
