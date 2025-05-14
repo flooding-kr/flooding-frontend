241bb9cb537addb7863129f0b6e0a2c1ef123979
민우석 <s23044@gsm.hs.kr>
2025-05-15 00:10:14 +0900

    💚 Fix: massage props

--- src/widgets/dormitory/ui/massage/header/index.tsx
+++ src/widgets/dormitory/ui/massage/header/index.tsx
@@ -7,9 +7,11 @@ import { Massage } from '@/shared/assets/icons';
 function MassageHeader() {
   return (
     <header className="flex items-center w-full">
-      <div className="flex items-center gap-6">
-        <Massage size={40} />
-        <p className="text-title3B text-black">안마의자 신청</p>
+      <div className="flex items-center gap-6 mobile:gap-2">
+        <div className="w-10 h-10 mobile:w-6 mobile:h-6">
+          <Massage />
+        </div>
+        <p className="text-title3B text-black mobile:text-body2B">안마의자 신청</p>
       </div>
     </header>
   );
