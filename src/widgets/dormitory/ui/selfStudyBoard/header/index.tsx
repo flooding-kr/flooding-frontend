53d40f9770dfea0b9c71e45efa31c6eed18cc5ec
민우석 <s23044@gsm.hs.kr>
2025-05-15 00:17:45 +0900

    🍱 Add: dormitory icon

--- src/widgets/dormitory/ui/selfStudyBoard/header/index.tsx
+++ src/widgets/dormitory/ui/selfStudyBoard/header/index.tsx
@@ -10,11 +10,15 @@ function SelfStudyHeader() {
 
   return (
     <header className="flex flex-1 justify-between items-center w-full">
-      <div className="flex items-center gap-6">
-        <Book size={40} />
-        <p className="text-title3B text-black">자습 신청</p>
+      <div className="flex items-center gap-6 mobile:gap-2">
+        <div className="w-10 mobile:w-6">
+          <Book />
+        </div>
+        <p className="text-title3B text-black mobile:text-body2B">자습 신청</p>
       </div>
-      <FilterButton onClick={() => setSelected(!selected)} select={selected} />
+      <div className="mobile:hidden">
+        <FilterButton onClick={() => setSelected(!selected)} select={selected} />
+      </div>
     </header>
   );
 }
