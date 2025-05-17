7fbbe00092419c9138c7c7d8ac86b9002a4efe70
민우석 <s23044@gsm.hs.kr>
2025-05-18 02:01:08 +0900

    🎨 Add: massage board

--- src/widgets/dormitory/ui/massage/index.tsx
+++ src/widgets/dormitory/ui/massage/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ReservationList from '../reservationList';
 import MassageFooter from './footer';
 import MassageHeader from './header';
+import { useFetchMassageList } from '../../model/useFetchMassageList';
 
 function Massage() {
+  const { massage, fetchMassage } = useFetchMassageList();
+
+  useEffect(() => {
+    fetchMassage();
+  }, []);
   return (
-    <div className="flex flex-1 flex-col self-stretch gap-8 w-full">
-      <div className="flex flex-1 flex-col self-stretch gap-6 w-full">
+    <div className="flex flex-1 flex-col self-stretch gap-8 w-full mobile:gap-6">
+      <div className="flex flex-1 flex-col self-stretch gap-6 w-full mobile:gap-4">
         <MassageHeader />
-        <ReservationList type="massage" />
+        <ReservationList type="massage" data={massage} />
       </div>
       <MassageFooter />
     </div>
--- src/widgets/dormitory/ui/reservationList/index.tsx
+++ src/widgets/dormitory/ui/reservationList/index.tsx
@@ -1,35 +1,59 @@
-import React from 'react';
-
 import StudentItem from '@/entities/dormitory/ui/StudentItem';
+import { Error } from '@/shared/assets/icons';
+import { MassageType } from '@/shared/types/dormitory';
 
 interface Props {
   type: 'selfStudy' | 'massage';
+  data: MassageType[];
 }
 
-function ReservationList({ type }: Props) {
-  const mock = [
-    {
-      student_number: '20230004',
-      name: '최민정',
-      profile_image_url: '',
-    },
-  ];
+function ReservationList({ type, data }: Props) {
+  const isSelfStudy = type === 'selfStudy';
+  const hasData = data?.length > 0;
+
+  const containerClass = isSelfStudy
+    ? 'grid gap-x-7 gap-y-6 mt-6 mobile:m-0 mobile:gap-x-[10px] mobile:gap-y-3 grid-cols-[repeat(auto-fill,minmax(120px,1fr))] mobile:grid-cols-[repeat(auto-fill,minmax(74px,1fr))]'
+    : 'flex gap-x-4 mt-6 mobile:mt-0';
+
+  const itemClass = isSelfStudy
+    ? 'relative overflow-visible'
+    : 'shrink-0 w-[120px] mobile:w-[74px]';
 
   return (
     <div
-      className={`w-[1360px] bg-white flex flew-1 flex-col px-7 py-6 rounded-lg overflow-y-auto custom-scrollbar pr-2 tablet:w-full ${type === 'selfStudy' ? 'h-[498px]' : 'h-[182px]'}`}
+      className={`w-[1360px] bg-white px-7 pb-6 rounded-lg tablet:w-full mobile:px-2 mobile:py-3
+        ${type === 'selfStudy' ? 'h-[498px] mobile:h-[264px]' : 'h-[182px] mobile:h-[152px]'}`}
     >
-      <div className="w-full flex flex-wrap gap-x-7 gap-y-6 mx-auto">
-        {mock.map((item, idx) => (
-          <StudentItem
-            type={type}
-            key={item.student_number}
-            stuImg={item.profile_image_url}
-            stuName={item.name}
-            stuNum={item.student_number}
-            rank={idx + 1}
-          />
-        ))}
+      <div className="relative h-full overflow-visible">
+        <div
+          className={`h-full pr-2 custom-scrollbar hide-scrollbar-mobile mobile:p-0
+            ${type === 'selfStudy' ? 'overflow-y-auto' : 'overflow-x-auto'}`}
+        >
+          {hasData ? (
+            <div className={containerClass}>
+              {data.map((item, idx) => (
+                <div key={item.school_number} className={itemClass}>
+                  <StudentItem
+                    type={type}
+                    stuImg={item.profile_image.presigned_url}
+                    stuName={item.name}
+                    stuNum={item.school_number}
+                    rank={idx + 1}
+                  />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="w-full h-full flex items-center justify-center">
+              <div className="flex justify-center items-center gap-4">
+                <div className="w-9 h-9 mobile:w-6 mobile:h-6">
+                  <Error color="#A7A7A7" />
+                </div>
+                <p className="text-body1R text-gray-500 mobile:text-body2R">신청자가 없습니다.</p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
