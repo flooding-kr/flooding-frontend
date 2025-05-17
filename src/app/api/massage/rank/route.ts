a760573b2c0f9cacb399c440fa195bf8bc3557e7
민우석 <s23044@gsm.hs.kr>
2025-05-18 01:25:45 +0900

    ✨ Add: dormitory apply rank

---
+++ src/app/api/massage/rank/route.ts
@@ -0,0 +1,22 @@
+import { AxiosError } from 'axios';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+import { apiClient } from '@/shared/libs/apiClient';
+
+export async function GET(request: Request) {
+  const accessToken = cookies().get('accessToken')?.value;
+  const headers = accessToken ? { Authorization: `Bearer ${accessToken}` } : undefined;
+
+  try {
+    const response = await apiClient.get(`/massage/rank`, { headers });
+    return NextResponse.json(response.data);
+  } catch (error) {
+    const axiosError = error as AxiosError<{ reason: string }>;
+
+    const status = axiosError.response?.status || 500;
+    const message = axiosError.response?.data?.reason || 'get failed';
+
+    return NextResponse.json({ error: message }, { status });
+  }
+}
