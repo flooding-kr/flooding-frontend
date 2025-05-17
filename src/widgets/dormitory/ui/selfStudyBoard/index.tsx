74c26e78a80f1746d4ea58522cb87d204bf47253
민우석 <s23044@gsm.hs.kr>
2025-05-18 02:06:41 +0900

    🎨 Add: self study board

--- src/widgets/dormitory/ui/selfStudyBoard/index.tsx
+++ src/widgets/dormitory/ui/selfStudyBoard/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import SelfStudyFooter from './footer';
 import SelfStudyHeader from './header';
+import { useFetchSelfStudyList } from '../../model/useFetchSelfStudyList';
 import ReservationList from '../reservationList';
 
 function SelfStudy() {
+  const { selfStudy, fetchSelfStudy } = useFetchSelfStudyList();
+
+  useEffect(() => {
+    fetchSelfStudy();
+  }, []);
   return (
-    <div className="flex flex-1 flex-col self-stretch gap-8 w-full">
-      <SelfStudyHeader />
-      <ReservationList type="selfStudy" />
+    <div className="flex flex-1 flex-col self-stretch gap-8 w-full mobile:gap-6">
+      <div className="flex flex-1 flex-col self-stretch gap-6 w-full mobile:gap-4">
+        <SelfStudyHeader />
+        <ReservationList type="selfStudy" data={selfStudy} />
+      </div>
       <SelfStudyFooter />
     </div>
   );
