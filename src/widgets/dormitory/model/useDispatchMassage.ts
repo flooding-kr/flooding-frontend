94406cb2aa18be8dbfc6794ef754b8e0d8e4a5e6
민우석 <s23044@gsm.hs.kr>
2025-05-19 21:04:04 +0900

    ✨ Add: reFetch

--- src/widgets/dormitory/model/useDispatchMassage.ts
+++ src/widgets/dormitory/model/useDispatchMassage.ts
@@ -1,12 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 
+import { useFetchMassage } from '@/widgets/home/model/useFetchMassage';
+
 import { postMassage } from '../api/postMassage';
 
 function useDispatchMassage() {
+  const { fetchMassage } = useFetchMassage();
+
   return useMutation({
     mutationFn: () => postMassage(),
     onSuccess: () => {
+      fetchMassage();
       toast.success('안마의자 신청에 성공하였습니다.');
     },
     onError: () => {
