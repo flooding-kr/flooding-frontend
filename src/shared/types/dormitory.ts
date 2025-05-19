978b18897a495900e97402ca6f8c632b57224766
민우석 <s23044@gsm.hs.kr>
2025-05-19 21:11:47 +0900

    🏷️ Fix: type

--- src/shared/types/dormitory.ts
+++ src/shared/types/dormitory.ts
@@ -1,18 +1,9 @@
 import { Image } from './image';
 
 export interface MassageType {
-  school_number: string;
-  name: string;
-  profile_image: Image;
-}
-
-export interface SelfStudyType {
   id: string;
-  student: SelfStudyStudent;
-}
-
-export interface SelfStudyStudent {
-  student_number: string;
+  student_number?: string;
   name: string;
   profile_image: Image;
+  school_number?: string;
 }
