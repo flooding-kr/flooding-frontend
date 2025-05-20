31039ccf384a6ae1cda6444b0cab60e21b5ce609
민우석 <s23044@gsm.hs.kr>
2025-05-20 16:52:00 +0900

    📱 Add: tablet responsive

--- src/widgets/home/ui/OverviewPanel/index.tsx
+++ src/widgets/home/ui/OverviewPanel/index.tsx
@@ -7,10 +7,16 @@ export default function OverviewPanel() {
   const user = useUser();
 
   return (
-    <div className="w-full flex flex-1 gap-4 justify-center mobile:flex-col mobile:items-center">
-      <ScheduleBoard />
-      <MealBoard />
-      <ProfileContainer src="" stuName={user?.name} stuNum={user?.stuNum} />
+    <div className="w-full flex gap-4 justify-center items-center tablet:flex-col tablet:items-start tablet:gap-4">
+      <div className="w-full tablet:flex-1">
+        <ScheduleBoard />
+      </div>
+      <div className="w-full tablet:flex-1">
+        <MealBoard />
+      </div>
+      <div className="tablet:hidden w-full">
+        <ProfileContainer src="" stuName={user?.name} stuNum={user?.stuNum} />
+      </div>
     </div>
   );
 }
