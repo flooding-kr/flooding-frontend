335587530acac0149222912a15456d2ea649ca95
민우석 <s23044@gsm.hs.kr>
2025-05-20 15:17:26 +0900

    ✏️ Fix: flooding typo

--- src/entities/signIn/ui/SigninPrompt/index.tsx
+++ src/entities/signIn/ui/SigninPrompt/index.tsx
@@ -8,7 +8,7 @@ export default function SigninPrompt() {
     <div className="flex mt-[10px] items-center justify-between">
       <div className="flex gap-4 items-center mobile:gap-[10px]">
         <p className="text-caption1R text-gray-300 mobile:text-caption2M">
-          아직 프러딩을 로그인 안 하셨나요?
+          아직 플러딩을 로그인 안 하셨나요?
         </p>
         <VerticalLine />
         <Link href="/signup" className="text-caption1B text-gray-500 mobile:text-caption2B">
