8e9014e59a98d84cd00182ddf22635c60e590f5a
민우석 <s23044@gsm.hs.kr>
2025-05-21 14:52:20 +0900

    ⚡️ Add: meal cache

--- src/app/_provider/TanstackProvider.tsx
+++ src/app/_provider/TanstackProvider.tsx
@@ -1,11 +1,28 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React, { useState } from 'react';
 
 function TanstackProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60,
+            gcTime: 1000 * 60 * 5,
+            refetchOnWindowFocus: true,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default TanstackProvider;
