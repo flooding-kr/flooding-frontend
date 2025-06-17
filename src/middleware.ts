bde8ef78108c1171f6568adaff113d27af4f1585
민우석 <s23044@gsm.hs.kr>
2025-06-17 16:19:27 +0900

    🐛 Fix: kst to utc

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,53 +1,62 @@
+import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value;
   const refreshToken = request.cookies.get('refreshToken')?.value;
+  const user = request.cookies.get('user')?.value;
 
   if (!refreshToken) {
     return NextResponse.redirect(new URL('/signin', request.url));
   }
 
-  if (!accessToken) {
+  if (!accessToken || !user) {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/re-issue`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          'Refresh-Token': `Bearer ${refreshToken}`,
-        },
-        credentials: 'include',
+      const res = await axios.patch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/auth/re-issue`,
+        {},
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Refresh-Token': `Bearer ${refreshToken}`,
+          },
+        }
+      );
+
+      const userRes = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user/myself`, {
+        headers: { Authorization: `Bearer ${res.data.access_token}` },
       });
 
-      if (!res.ok) {
-        return NextResponse.redirect(new URL('/signin', request.url));
-      }
-
-      const data = await res.json();
+      const encodedUser = Buffer.from(JSON.stringify(userRes.data)).toString('base64');
 
       const response = NextResponse.next();
 
-      response.cookies.set('accessToken', data.access_token, {
+      response.cookies.set('accessToken', res.data.access_token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        expires: new Date(`${data.access_token_expired_at}Z`),
+        expires: new Date(new Date(`${res.data.access_token_expired_at}`).toUTCString()),
         sameSite: 'strict',
       });
 
-      response.cookies.set('refreshToken', data.refresh_token, {
+      response.cookies.set('refreshToken', res.data.refresh_token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        expires: new Date(`${data.refresh_token_expired_at}Z`),
+        expires: new Date(new Date(`${res.data.refresh_token_expired_at}`).toUTCString()),
         sameSite: 'strict',
       });
 
+      response.cookies.set('user', encodedUser, {
+        httpOnly: false,
+        secure: process.env.NODE_ENV === 'production',
+        expires: new Date(new Date(`${res.data.access_token_expired_at}`).toUTCString()),
+        sameSite: 'strict',
+      });
       return response;
     } catch (err) {
       console.error('토큰 재발급 실패:', err);
       return NextResponse.redirect(new URL('/signin', request.url));
     }
   }
-
   return NextResponse.next();
 }
 
