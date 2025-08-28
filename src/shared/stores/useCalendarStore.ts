5a986ba8506518a2ce7eaaa6d6a1a15fbe0498a4
민우석 <s23044@gsm.hs.kr>
2025-08-28 17:17:15 +0900

    🐛 Fix: data reset

--- src/shared/stores/useCalendarStore.ts
+++ src/shared/stores/useCalendarStore.ts
@@ -2,12 +2,16 @@ import { create } from 'zustand';
 
 interface State {
   date: string;
+  currentDate: Date;
   setDate: (date: string) => void;
+  setCurrentDate: (currentDate: Date) => void;
 }
 
 const useCalendarStore = create<State>(set => ({
   date: '',
+  currentDate: new Date(),
   setDate: date => set({ date }),
+  setCurrentDate: currentDate => set({ currentDate }),
 }));
 
 export default useCalendarStore;
