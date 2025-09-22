31d1416f9329e6c1d697d3ecfe9f9887312d135f
민우석 <s23044@gsm.hs.kr>
2025-09-22 14:57:57 +0900

    💚 Fix: conflict problem

--- src/shared/ui/FilterButton/index.tsx
+++ src/shared/ui/FilterButton/index.tsx
@@ -5,22 +5,23 @@ import React from 'react';
 import { Filter } from '@/shared/assets/icons';
 
 interface Props {
-  select: boolean;
+  select?: boolean;
   onClick: () => void;
 }
 
 export default function FilterButton({ onClick, select }: Props) {
   let buttonStyle =
-    'whitespace-nowrap py-3 px-3 rounded-xl text-body2R flex gap-3 mobile:text-body2R ';
+    'whitespace-nowrap py-3 px-3 rounded-xl text-body2R flex items-center gap-3 mobile:text-body3R mobile:px-2 mobile:py-1 ';
 
   if (select) {
-    buttonStyle += 'border border-solid border-main-600 bg-main-600 bg-gray-400 text-white';
+    buttonStyle += 'border border-solid border-main-600 bg-main-600 text-white';
   } else {
     buttonStyle += 'border border-solid border-main-600 text-main-600';
   }
+
   return (
     <button type="button" className={buttonStyle} onClick={onClick}>
-      <div className="w-6 h-6">
+      <div className="w-6 h-6 mobile:w-4 mobile:h-4">
         <Filter select={select} />
       </div>
       필터
